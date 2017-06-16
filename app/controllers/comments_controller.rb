8a87262063178e78aeaae3c726658b629393a36d
Drew Pham <drewvpham@gmail.com>
2017-06-16 15:34:33 -0500

    more progress

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @user=User.find(session[:user_id])
+    @event=Event.find(params[:id])
+    comment=Comment.create(user:@user.id, event:@event)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'users#new'
   get '/user/:id'=> 'users#show'
   post '/user/:id'=> 'users#update'
+  post '/comments' => 'comments#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
